4292cd8a4d47e127cbdc216c97dfce3f0997342a
anna <anna.bogomiagkova@codeit.com.ua>
2018-09-04 13:44:15 +0300

    clean old db data each time server is launched

--- src/index.js
+++ src/index.js
@@ -52,13 +52,10 @@ io.on("connection", socket => {
     })
 });
 
-
+clearPrices(); //clean data older than a 24h
 setInterval(()=>
     axios.get(`https://api.binance.com/api/v1/ticker/24hr`).then(response => {
         savePrices(response.data);
     }).catch(error => {console.log(error);}), 1000*60*process.env.ADD_TO_DB_INTERVAL) //add new info every 5min
 
-setInterval(()=>
-    axios.get(`https://api.binance.com/api/v1/ticker/24hr`).then(response => {
-        clearPrices(response.data);
-    }).catch(error => {console.log(error);}), 1000*60*process.env.CLEAN_DB_INTERVAL)//clean data ones a day
+setInterval(()=>clearPrices(), 1000*60*process.env.CLEAN_DB_INTERVAL)//clean data ones a day
